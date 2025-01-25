79913b03a87ff7d5c55fd0ef0e3192da3baf795b
laurin42 <laurin.schmidt@yahoo.com>
2025-01-25 23:15:41 +0100

    yes

---
+++ app/components/ui/TarotCard.tsx
@@ -0,0 +1,9 @@
+import { View, Image } from "react-native";
+
+export default function TarotCard();
+return;
+<View>
+  <Image source={require("@/assets/images/TarotCards")} style={style} />
+</View>;
+
+const styles = StyleSheet.create();
