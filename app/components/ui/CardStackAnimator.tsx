7cba4ee15dab882942745e24833742ae29d32d1e
laurin42 <laurin.schmidt@yahoo.com>
2025-01-26 02:04:34 +0100

    card drawing

---
+++ app/components/ui/CardStackAnimator.tsx
@@ -0,0 +1,108 @@
+import React, { useRef, useState } from "react";
+import {
+  Animated,
+  View,
+  StyleSheet,
+  Pressable,
+  Text,
+  Dimensions,
+  Image,
+} from "react-native";
+
+export default function CardStackAnimator({ onAnimationComplete }) {
+  const [activeCards, setActiveCards] = useState([]);
+  const translateYValues = useRef(
+    Array(3)
+      .fill(0)
+      .map(() => new Animated.Value(Dimensions.get("window").height))
+  ).current;
+
+  const pullCards = () => {
+    const animations = translateYValues.map((value, index) =>
+      Animated.timing(value, {
+        toValue: 0,
+        duration: 500,
+        delay: index * 300,
+        useNativeDriver: true,
+      })
+    );
+
+    Animated.sequence(animations).start(() => {
+      setActiveCards([0, 1, 2]);
+      onAnimationComplete && onAnimationComplete();
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      {translateYValues.map((value, index) => (
+        <Animated.View
+          key={index}
+          style={[
+            styles.card,
+            {
+              transform: [
+                { translateY: value },
+                { rotate: `${index * 16}deg` },
+              ],
+              top: index * +70,
+            },
+          ]}
+        >
+          <View style={styles.container}>
+            <Image
+              source={require("@/assets/images/tarot_cards/Card_back.png")}
+              style={styles.cardBack}
+            />
+          </View>
+        </Animated.View>
+      ))}
+      {activeCards.length === 0 && (
+        <Pressable style={styles.startButton} onPress={pullCards}>
+          <Text style={styles.startText}>PULL CARDS</Text>
+        </Pressable>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  card: {
+    position: "absolute",
+    width: 200,
+    height: 300,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    backgroundColor: "transparent",
+  },
+  cardBack: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+    backgroundColor: "gray", // Rückseite der Karte
+    borderWidth: 2,
+    borderColor: "white",
+  },
+  startButton: {
+    position: "absolute",
+    bottom: 50,
+    backgroundColor: "red",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+  },
+  startText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
