bcfa6b0fcf144f032f6734634017f61d55a951e8
atoczynski <artur.torcz@gmail.com>
2025-01-26 02:52:34 +0100

    Add database configuration and schema for Tarot cards and users

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: "flex",
     backgroundColor: "black",
     justifyContent: "center",
     alignItems: "center",
