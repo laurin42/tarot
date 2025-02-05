e32189a6ead38ca757726f8c0ff02d98f3716012
laurin42 <laurin.schmidt@yahoo.com>
2025-02-05 13:23:51 +0100

    gluestack install

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,8 +1,16 @@
 module.exports = function (api) {
     api.cache(true);
+    api.cache(true);
     return {
-        presets: [
-            ["babel-preset-expo"]
-        ],
+        presets: [["babel-preset-expo"], "nativewind/babel"],
+
+        plugins: [["module-resolver", {
+            root: ["./"],
+
+            alias: {
+                "@": "./",
+                "tailwind.config": "./tailwind.config.js"
+            }
+        }]]
     };
 };
